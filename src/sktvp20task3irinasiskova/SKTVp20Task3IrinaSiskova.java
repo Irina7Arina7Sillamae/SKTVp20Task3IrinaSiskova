36c76bbc8c8950955e0c6eed8835bf5435903731
irina Siskova <Irina.Siskova@ivkhk.ee>
2021-09-29 00:00:43 +0300

    correct

--- src/sktvp20task3irinasiskova/SKTVp20Task3IrinaSiskova.java
+++ src/sktvp20task3irinasiskova/SKTVp20Task3IrinaSiskova.java
@@ -12,28 +12,28 @@ public class SKTVp20Task3IrinaSiskova {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-	System.out.println("Ведите Ваше имя:  " );
+	System.out.println("Введите Ваше имя:  " );
 	String phrase1 = sc.nextLine();
         
-        System.out.println("Ведите Вашу фамилию:  " );
+        System.out.println("Введите Вашу фамилию:  " );
         String phrase2 = sc.nextLine();
         
-        System.out.println("Ведите год вашего рождения:  " );
+        System.out.println("Введите день Вашего рождения:  " );
         String phrase3 = sc.nextLine();
         
-        System.out.println("Ведите месяц вашего рождения:  " );
+        System.out.println("Введите месяц вашего рождения:  " );
         String phrase4 = sc.nextLine();
         
-        System.out.println("Ведите день вашего рождения:  " );
+        System.out.println("Ведите год вашего рождения:  " );
         String phrase5 = sc.nextLine();
         
         
         
-	System.out.println(phrase1 + " " + phrase2 + " " + phrase3 + " " + phrase4 + " " + phrase5 + " "
-                + "ВОТ!!!");
+	System.out.println(phrase1 + " " + phrase2 + " дата Вашего рождения: " + 
+                phrase3 + " " + phrase4 + " " + phrase5 + " год.\n" + "            ВОТ!!!");
         
         
-        
+      
     }
     
 }
