1116d9c097c9585edce338861b0d34d256a95f9d
irina Siskova <Irina.Siskova@ivkhk.ee>
2021-09-28 20:18:48 +0300

    zadanie3

---
+++ src/sktvp20task3irinasiskova/SKTVp20Task3IrinaSiskova.java
@@ -0,0 +1,39 @@
+
+package sktvp20task3irinasiskova;
+
+import java.util.Scanner;
+
+
+
+
+public class SKTVp20Task3IrinaSiskova {
+
+   
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+	System.out.println("Ведите Ваше имя:  " );
+	String phrase1 = sc.nextLine();
+        
+        System.out.println("Ведите Вашу фамилию:  " );
+        String phrase2 = sc.nextLine();
+        
+        System.out.println("Ведите год вашего рождения:  " );
+        String phrase3 = sc.nextLine();
+        
+        System.out.println("Ведите месяц вашего рождения:  " );
+        String phrase4 = sc.nextLine();
+        
+        System.out.println("Ведите день вашего рождения:  " );
+        String phrase5 = sc.nextLine();
+        
+        
+        
+	System.out.println(phrase1 + " " + phrase2 + " " + phrase3 + " " + phrase4 + " " + phrase5 + " "
+                + "ВОТ!!!");
+        
+        
+        
+    }
+    
+}
